d1723f9c0eb271e632817faf950a5bbcc3317f95
Dmitrii Govor <dmitrii.m.govor@gmail.com>
2023-03-15 05:21:38 -0700

    Test commit message

---
+++ Practice #1/1.sql
@@ -0,0 +1,12 @@
+/*
+Created By: Dmitrii Govor
+Create Date: 03/15/2023
+Description: This query displays all customers first, last names and email addresses
+*/
+
+SELECT 
+	FirstName, 
+	LastName, 
+	Email
+FROM 
+	Customer
