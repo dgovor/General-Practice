ed94a9cc2b744840a8823caa5100a3c4f5914700
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2023-03-16 03:25:49 -0700

    Update 1.sql

--- Practice #1/1.sql
+++ Practice #1/1.sql
@@ -4,10 +4,13 @@ Create Date: 03/15/2023
 Description: This query displays all customers first, last names and email addresses
 */
 
-SELECT 
-	FirstName, 
-	LastName, 
-	Email
-FROM 
+SELECT
+	FirstName AS 'Customer First Name',
+	LastName AS 'Customer Last Name',
+	Email AS 'Customer Email'
+FROM
 	Customer
-	
+ORDER BY
+	FirstName ASC,
+	LastName ASC
+LIMIT 10
