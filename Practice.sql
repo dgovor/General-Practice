7c4f380481be352b2a55f03fd02934f6238bf7fa
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2023-03-17 03:58:33 -0700

    Update Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -1,17 +1,23 @@
 /*
 Created By: Dmitrii Govor
 Create Date: 03/15/2023
-Description: How many invoices were billed to Brussels, Orlando or Paris?
+Description:
 */
 
 SELECT 
 	InvoiceDate,
 	BillingAddress,
 	BillingCity,
-	total
+	total,
+	CASE
+		WHEN total < 2.00 THEN 'Baseline Purchase'
+		WHEN total BETWEEN 2.00 AND 6.99 THEN 'Low Purchase'
+		WHEN total BETWEEN 7.00 AND 15.00 THEN 'Target Purchase'
+		ELSE 'Top Performer'
+	END AS PurchaseType
 FROM
 	Invoice
 WHERE
-	BillingCity IN ('Brussels', 'Orlando', 'Paris')
+	PurchaseType = 'Top Performer'
 ORDER BY
 	InvoiceDate DESC
